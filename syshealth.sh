3232995046e543ef7920cddf81f2f620c7712808
Loujain-Alnounou <lma1669@rit.edu>
2026-09-01 15:30:47 -0400

    Add shebang and make syshealth.sh executable

---
+++ syshealth.sh
@@ -0,0 +1,9 @@
+# !/usr/bin/env bash
+#Note: the shebang above is the first line of the script that tells 
+	#the computer to run this file using bash   
+# ==============================================
+# syshealth.sh - System Health & Log Analysis Toolkit
+# Lab 1 - Data Collector
+# Author: Your Name
+# Date: $(date +%Y-%m-%d)
+# ==============================================
